10cc262e432738a1b7406de30c20bf4c3b3c1a10
marcospedroche05 <marcospedroche62@gmail.com>
2025-10-27 09:34:57 +0100

    par impar vue

--- src/Router.js
+++ src/Router.js
@@ -3,20 +3,15 @@ import NombreComponent from "./components/NombreComponent.vue";
 import EdadComponent from "./components/EdadComponent.vue";
 import CicloVida from "./components/CicloVida.vue";
 import DirectivasComponent from "./components/DirectivasComponent.vue";
-<<<<<<< HEAD
+
 import ParImpar from "./components/ParImpar.vue";
-=======
->>>>>>> 497c05c8a3cf998ef85f74765f16d6f9d8f640a5
 
 const myRoutes = [
   { path: "/", component: NombreComponent },
   { path: "/edad", component: EdadComponent },
   { path: "/contador", component: CicloVida },
   { path: "/directivas", component: DirectivasComponent },
-<<<<<<< HEAD
   { path: "/parimpar", component: ParImpar },
-=======
->>>>>>> 497c05c8a3cf998ef85f74765f16d6f9d8f640a5
 ];
 
 const router = createRouter({
--- src/components/ParImpar.vue
+++ src/components/ParImpar.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
     <h1>Par Impar</h1>
-    <label>Introduce un numero</label>
-    <input type="number"/>
+    <label>Introduce un numero: </label>
+    <input type="number" v-model.number="numero" @change="compruebaNumero()" />
+    <button v-on:click="generaRandom()">Generar aleatorio</button>
+    <!-- <h2 v-if="numero % 2 == 0">Es par</h2>
+    <h2 v-else>Es impar</h2> -->
+    <h2>{{ mensaje }}</h2>
   </div>
 </template>
 <script>
 export default {
   name: "ParImpar",
-  data(){
+  data() {
     return {
-        
-    }
-  }
+      numero: 0,
+      mensaje: "",
+    };
+  },
+  methods: {
+    generaRandom() {
+      var random = parseInt(Math.random() * 100 + 1);
+      this.numero = random;
+      this.compruebaNumero();
+    },
+    compruebaNumero() {
+      if (this.numero % 2 == 0) {
+        this.mensaje = "Es par";
+      } else {
+        this.mensaje = "Es impar";
+      }
+    },
+  },
 };
 </script>
 <style>
